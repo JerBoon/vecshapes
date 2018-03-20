8279f359f2bf00822e3cccdfa3d6507d845364ef
Jer Boon <jer.booner@gmail.com>
2018-03-20 09:20:16 +0000

    Initial commit with dodecahedron constructor

---
+++ R/dodec.R
@@ -0,0 +1,100 @@
+
+#--------------------------------------------------------------------------------
+# Library of various 3D shapes
+#--------------------------------------------------------------------------------
+
+
+#---------------------------------------------------------------------
+#' Return a dodecahedron  object, constructed from elementary spacial objects
+#'
+#' @param centre Centre of dodecahedron 
+#' @param size TBA
+#' @param properties Package-independent object defining additional properties
+#' @param bound Include a bounding sphere? Default=TRUE
+#' @param align.to Name of axis to align to - i.e. 2 surfaces will be level in that plane. Default = "x"
+#'
+#' @return A compound object, comprising of elementary triangle objects
+#'   in a grouped hierarchical object, with a surrounding bounding sphere
+#'   used for spacial indexing.
+#' 
+#' @export
+#' @importFrom vecspace Spc.MakePolygon Spc.Combine Spc.Translate Spc.Rotate
+#'
+#' @family constructors
+#'
+#' @examples
+#'   w <- Spc.MakeDodecahedron(c(0,0,0), 10, surface_props)
+
+Spc.MakeDodecahedron <- function (centre, size, properties=NA, bound=TRUE, align.to="x") {
+
+  if ((typeof(centre) != "double") || (length(centre) != 3)) {
+    print("Spc.MakeDodecahedron: centre should be a 3 number vector")
+    return(NA)
+  }
+
+  if ((typeof(size) != "double") || (length(size) != 1)|| (size <= 0)) {
+    print("Spc.MakeDodecahedron: size should be a positive scalar numeric")
+    return(NA)
+  }
+
+  #----
+
+  #Definition of vertices, from https://en.wikipedia.org/wiki/Dodecahedron#Cartesian_coordinates
+
+  phi <- (1 +sqrt(5))/2
+
+  verts <- matrix(c(1,1,1,1,1,-1,1,-1,1,1,-1,-1,-1,1,1,-1,1,-1,-1,-1,1,-1,-1,-1,
+                    0,phi,1/phi,0,phi,-1/phi,0,-phi,1/phi,0,-phi,-1/phi,
+                    1/phi,0,phi,1/phi,0,-phi,-1/phi,0,phi,-1/phi,0,-phi,
+                    phi,1/phi,0,phi,-1/phi,0,-phi,1/phi,0,-phi,-1/phi,0),
+                  ncol=3, byrow=TRUE)
+
+  faces <- matrix(c(9,1,17,2,10,
+                    20,8,12,11,7,
+                    18,3,11,12,4,
+                    10,6,19,5,9,
+                    2,17,18,4,14,
+                    17,1,13,3,18,
+                    6,16,8,20,19,
+                    19,20,7,15,5,
+                    10,2,14,16,6,
+                    13,1,9,5,15,
+                    11,3,13,15,7,
+                    14,4,12,8,16),
+                 ncol=5, byrow=TRUE)
+
+  to.face <- function (v) {
+    return(Spc.MakePolygon(verts[v,1],verts[v,2],verts[v,3]))
+  }
+
+  #----
+
+  r <- list()
+
+  for (i in 1:12)
+    r <- append(r, to.face(faces[i,]))
+
+  r <- Spc.Combine(r, properties=properties, bound=bound)
+
+  r <- Spc.Translate(r, centre)
+
+  #---- rotations to plane
+
+  dihangle <- (pi - atan(2)) *180 / pi
+
+  if (align.to %in% c("x","X"))
+    r <- Spc.Rotate(r, ,pivot.angle=c(0,0,dihangle/2))
+
+  if (align.to %in% c("y","Y"))
+    r <- Spc.Rotate(r, ,pivot.angle=c(dihangle/2,0,0))
+
+  if (align.to %in% c("z","Z"))
+    r <- Spc.Rotate(r, ,pivot.angle=c(0,dihangle/2,0))
+
+  return(r)
+}
+
+
+
+
+
